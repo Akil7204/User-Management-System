cc1e87344e8d03d3139f357b289caa025cb1cce0
Akil <akilakil6874@gmail.com>
2024-05-03 14:47:42 +0530

    Created backend and run the server

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div className='bg-slate-300'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
             <Link to="/"> 
                 <h1 className='font-bold'>Mern app</h1>
             </Link>
