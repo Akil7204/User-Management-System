a7ae8df8770aa960ffae20c218b1770c8771ca53
Akil <akilakil6874@gmail.com>
2024-05-03 16:49:57 +0530

    connected to db

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,13 @@
-
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to mongodb");
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
